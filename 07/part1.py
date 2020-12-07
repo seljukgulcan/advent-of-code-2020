1349ce7d8c7a8fe98c26370ac9abb471917f58ec
Shathra <selcuk.gulcan@bilkent.edu.tr>
2020-12-07 08:47:43 +0300

    day 07

---
+++ 07/part1.py
@@ -0,0 +1,65 @@
+filename = 'input.txt'
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.edge_lst = []  # edge is a tuple of (node, weight)
+        self.parent_lst = []
+
+    def add_node(self, node, weight):
+        self.edge_lst.append((node, weight))
+        node.parent_lst.append((self, weight))
+
+
+rhs_set = set()
+lhs_set = set()
+name2node = dict()
+
+with open(filename) as file:
+    for line in file:
+        line = line.strip()
+        lhs, rest = line.split(' contain ')
+
+        if rest == 'no other bags.':
+            continue
+
+        lhs_set.add(lhs)
+        if lhs not in name2node:
+            name2node[lhs] = Node(lhs)
+
+        lhs_node = name2node[lhs]
+
+        rest = rest[:-1].split(', ')
+        for bag_info in rest:
+            count, rhs = bag_info.split(maxsplit=1)
+            count = int(count)
+
+            if count == 1:
+                rhs = rhs + 's'
+
+            rhs_set.add(rhs)
+            if rhs not in name2node:
+                name2node[rhs] = Node(rhs)
+
+            rhs_node = name2node[rhs]
+            lhs_node.add_node(rhs_node, count)
+
+
+root_set = lhs_set - rhs_set
+
+
+queue = ['shiny gold bags']
+visited = set()  # Already visited
+
+while queue:
+    name = queue.pop(0)
+
+    visited.add(name)
+
+    for parent_node, weight in name2node[name].parent_lst:
+        parent_name = parent_node.name
+
+        if parent_name not in visited:
+            queue.append(parent_name)
+
+print(len(visited) - 1)
