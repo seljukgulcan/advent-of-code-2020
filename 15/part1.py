0e20da1fb8cd10a28bcbbea9322f88afcbf4a24c
Shathra <selcuk.gulcan@bilkent.edu.tr>
2020-12-15 08:14:45 +0300

    day 15

---
+++ 15/part1.py
@@ -0,0 +1,26 @@
+filename = 'input.txt'
+
+lst = [0, 12, 6, 13, 20, 1]
+# lst = [0, 3]
+
+no2turn = {no: i + 1 for i, no in enumerate(lst)}
+
+last_no = 17
+# last_no = 6
+
+for i in range(len(lst) + 1, 2020 + 1):
+
+    answer = None
+
+    if last_no in no2turn:
+        age = i - no2turn[last_no]
+        answer = age
+    else:
+        answer = 0
+
+    no2turn[last_no] = i
+
+    last_no = answer
+
+    print('Turn {}: {}'.format(i + 1, answer))
+
